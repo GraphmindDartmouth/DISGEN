4dcc5b48a87257ea698f02fcc2854c194d6b8ea0
HUANG Zheng <38206045+TTTae@users.noreply.github.com>
2024-06-13 22:00:29 -0400

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,7 +1,6 @@
 # Execute the following scripts to use a gnn explainer and generate augmented graphs:
 cd DISGEN/src
 python gnnexp.py --dataset_root DISGEN/dataset  --train_val_test_idx_save_root DISGEN/saved_idx  --dataset bbbp# Train DISGEN on bbbp
-python disentgnn.py --dataset {dataset} --dataset_root {dataset_root}  --train_val_test_idx_save_root {train_val_test_idx_save_root} --criterion {criterion}
 
 # Execute the following scripts to train DISGEN:
-python disentgnn.py --dataset_root DISGEN/dataset  --train_val_test_idx_save_root DISGEN/saved_idx --saved_model DISGEN/saved_model --dataset bbbp --criterion pair_loss_cos
+python disentgnn.py --dataset_root DISGEN/dataset  --train_val_test_idx_save_root DISGEN/saved_idx --saved_model DISGEN/saved_model --dataset bbbp --criterion pair_loss_cos --beta1 0.5 --beta2 1 --beta3 5e4 --lr 0.001
